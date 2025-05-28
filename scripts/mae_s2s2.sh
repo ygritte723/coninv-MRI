0fb24fb6d7ae7751d6ac2cf3b1ce3b351bfc60d3
ygritte723 <ygritte0723@gmail.com>
2025-05-28 14:54:31 -0400

    restore

---
+++ scripts/mae_s2s2.sh
@@ -0,0 +1,3 @@
+nohup python trainV3.py \
+    --config cfg/s2s2/placenta_mae.yaml \
+    > logs/s2s2/placenta_mae.log 2>&1 &
---
+++ scripts/train.sh
@@ -0,0 +1,3 @@
+nohup python trainV3.py \
+    --config cfg/nips/nips_mae_test.yaml \
+    > logs/nips_mae_test.log 2>&1 &
